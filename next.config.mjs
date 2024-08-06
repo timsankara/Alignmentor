8bf0b24b03d3d8b859d2499a9ba5a5c91c9497f3
timsankara <chimpy.the.dev@outlook.com>
2024-08-07 02:37:08 +0300

    remove redirects in the next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: process.env.NEXT_PUBLIC_API_BASE_URL + '/api/:path*',
-      },
-    ];
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
